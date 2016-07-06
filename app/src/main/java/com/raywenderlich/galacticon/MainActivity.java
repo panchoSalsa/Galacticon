6b13683271aea46bf28b69497b48dd781a3c1d4f
Franco <frfranco@uci.edu>
2016-07-06 10:20:30 -0700

    completed tutorial

--- app/src/main/java/com/raywenderlich/galacticon/MainActivity.java
+++ app/src/main/java/com/raywenderlich/galacticon/MainActivity.java
@@ -27,7 +27,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.OnScrollListener;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -38,49 +37,145 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ImageRequester.ImageRequesterResponse {
 
-  private ArrayList<Photo> mPhotosList;
-  private ImageRequester mImageRequester;
+    private ArrayList<Photo> mPhotosList;
+    private ImageRequester mImageRequester;
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.menu_main, menu);
-    return true;
-  }
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    private RecyclerAdapter mAdapter;
 
+    private GridLayoutManager mGridLayoutManager;
 
-    mPhotosList = new ArrayList<>();
-    mImageRequester = new ImageRequester(this);
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-
-  }
-
-  private void requestPhoto() {
-
-    try {
-      mImageRequester.getPhoto();
-    } catch (IOException e) {
-      e.printStackTrace();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
-  }
 
-  @Override
-  public void receivedNewPhoto(final Photo newPhoto) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mPhotosList.add(newPhoto);
-      }
-    });
-  }
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        mGridLayoutManager = new GridLayoutManager(this, 2);
+
+        mPhotosList = new ArrayList<>();
+
+        mAdapter = new RecyclerAdapter(mPhotosList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        setRecyclerViewScrollListener();
+
+        setRecyclerViewItemTouchListener();
+
+        mImageRequester = new ImageRequester(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mPhotosList.size() == 0) {
+            requestPhoto();
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_recycler_manager) {
+            changeLayoutManager();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private int getLastVisibleItemPosition() {
+        int itemCount;
+
+        if (mRecyclerView.getLayoutManager().equals(mLinearLayoutManager)) {
+            itemCount = mLinearLayoutManager.findLastVisibleItemPosition();
+        } else {
+            itemCount = mGridLayoutManager.findLastVisibleItemPosition();
+        }
+
+        return itemCount;
+    }
+
+    private void setRecyclerViewScrollListener() {
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int totalItemCount = mRecyclerView.getLayoutManager().getItemCount();
+                if (!mImageRequester.isLoadingData() && totalItemCount == getLastVisibleItemPosition() + 1) {
+                    requestPhoto();
+                }
+            }
+        });
+    }
+
+    private void setRecyclerViewItemTouchListener() {
+
+        //1
+        ItemTouchHelper.SimpleCallback itemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder viewHolder1) {
+                //2
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                //3
+                int position = viewHolder.getAdapterPosition();
+                mPhotosList.remove(position);
+                mRecyclerView.getAdapter().notifyItemRemoved(position);
+            }
+        };
+
+        //4
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(itemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    private void requestPhoto() {
+
+        try {
+            mImageRequester.getPhoto();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void receivedNewPhoto(final Photo newPhoto) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mPhotosList.add(newPhoto);
+                mAdapter.notifyItemInserted(mPhotosList.size());
+            }
+        });
+    }
+
+    private void changeLayoutManager() {
+        if (mRecyclerView.getLayoutManager().equals(mLinearLayoutManager)) {
+            //1
+            mRecyclerView.setLayoutManager(mGridLayoutManager);
+            //2
+            if (mPhotosList.size() == 1) {
+                requestPhoto();
+            }
+        } else {
+            //3
+            mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        }
+    }
 }
